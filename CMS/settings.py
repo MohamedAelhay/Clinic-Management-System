cacbc077774c2df1d3ca11366c25501e50bc6964
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-22 17:04:08 +0200

    add templates and static dirs

--- CMS/settings.py
+++ CMS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "clinic/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'clinic/static/')
