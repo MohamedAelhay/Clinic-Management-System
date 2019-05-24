8a96f5ed1514f9f5c94146806d74380355e6316b
mariam-egm <mariam.gerges014@gmail.com>
2019-05-24 22:50:09 +0200

    Change styling

--- clinic/models.py
+++ clinic/models.py
@@ -14,7 +14,7 @@ class Patient(models.Model):
     patient_zip_code = models.IntegerField()
 
     def __str__(self):
-        return self.patient_first_name + self.patient_family_name
+        return self.patient_first_name + " " + self.patient_family_name
         
 class Doctor(models.Model):
     doctor_id = models.AutoField(primary_key=True)
@@ -24,7 +24,7 @@ class Doctor(models.Model):
     doctor_degree = models.CharField(max_length = 200)
 	
     def __str__(self):
-        return self.doctor_first_name + self.doctor_family_name
+        return self.doctor_first_name + " " + self.doctor_family_name
 
 
 class Visit(models.Model):
