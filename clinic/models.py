abfad385c8f5a0d6ac93ebd53e4f440db07bfb7c
mariam-egm <mariam.gerges014@gmail.com>
2019-05-24 22:41:11 +0200

    Add attending doctor, referring doctor, building, floor, room and bed to Visit model

--- clinic/models.py
+++ clinic/models.py
@@ -14,7 +14,7 @@ class Patient(models.Model):
     patient_zip_code = models.IntegerField()
 
     def __str__(self):
-        return self.patient_first_name
+        return self.patient_first_name + self.patient_family_name
         
 class Doctor(models.Model):
     doctor_id = models.AutoField(primary_key=True)
@@ -24,11 +24,16 @@ class Doctor(models.Model):
     doctor_degree = models.CharField(max_length = 200)
 	
     def __str__(self):
-        return self.doctor_first_name
+        return self.doctor_first_name + self.doctor_family_name
 
 
 class Visit(models.Model):
     visit_id = models.AutoField(primary_key=True)
     visit_date = models.DateTimeField()
     patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    attending_doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE,related_name='attending_doctor')
+    referring_doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE,related_name='referring_doctor')
+    building = models.IntegerField()
+    floor = models.IntegerField()
+    room = models.IntegerField()
+    bed = models.IntegerField()
